8354a74ba9e6fb6877c8a92f58110ecd8debbde9
Vansh Sukhija <12112021@nitkkr.ac.in>
2022-09-05 01:53:26 +0530

    Added update task feature

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // console.log("Hello World");
 
+function getData(){
+    url = 'https://api.unsplash.com/photos/random?page=1&query=nature&orientation=landscape&client_id=AM7OQGSHBU6vRpq32_eZve8Z62b767qv3Rrczf8ppZw'
+    fetch(url).then((response)=>{
+        // console.log(response);
+        return response.json();
+    }).then((data)=>{
+        // console.log(data['urls']['full']);
+        return data['urls']['full'];
+    })
+}
+
 let task = document.getElementsByClassName("task");
 let taskContainer = document.getElementById("taskContainer");
 let checkbtn = document.getElementsByClassName("checkbtn");
+let updatebtn = document.getElementsByClassName("updatebtn");
 let addbtn = document.getElementsByClassName("addbtn");
+let clrbtn = document.getElementsByClassName("clrbtn");
 let inpTask = document.getElementById("inpTask");
 let empty = document.getElementById("empty");
 let msg = document.getElementById("msg");
+
+document.querySelector('body').style.backgroundImage=`url('${String(getData())}')`;
 
 let todos = [];
 if(JSON.parse(localStorage.getItem("todos")).length){
@@ -18,7 +33,7 @@ if(JSON.parse(localStorage.getItem("todos")).length){
         taskContainer.innerHTML += `<div class="task" onmouseenter="showicons(this)" onmouseleave="hideicons(this)">
         <input type="checkbox" class="checkbtn" onclick="check(this)">
         <div class="description">${element}</div>
-        <i class="fa-regular fa-pen-to-square"></i>
+        <i class="fa-regular fa-pen-to-square" onclick="updatetask(this)"></i>
         <i class="fa-regular fa-trash-can" onclick="deltask(this)"></i>
         </div>`;
     });
@@ -38,11 +53,13 @@ function hideicons(index){
 
 function addtask(){
     if(inpTask.value){
+        clrbtn.disable = false;
+        // clrbtn.style.backgroundColor = "white";
         empty.style.display = "none";
         taskContainer.innerHTML += `<div class="task" onmouseenter="showicons(this)" onmouseleave="hideicons(this)">
         <input type="checkbox" class="checkbtn" onclick="check(this)">
         <div class="description">${inpTask.value}</div>
-        <i class="fa-regular fa-pen-to-square"></i>
+        <i class="fa-regular fa-pen-to-square" onclick="updatetask(this)"></i>
         <i class="fa-regular fa-trash-can" onclick="deltask(this)"></i>
         </div>`;
         todos.push(inpTask.value);
@@ -52,12 +69,25 @@ function addtask(){
         msganimate("Task Added");
     }
     else
-        alert("You cannot enter an empty task");
+        alert("Cannot enter an empty task");
+}
+
+function cleartask(){
+    clrbtn.disable = true;
+    // clrbtn.style.backgroundColor = "grey";
+    if(todos.length==0)
+        msganimate("There is no task to be cleared");
+        else
+        msganimate("Cleared all tasks");
+    localStorage.clear();
+    todos = [];
+    empty.style.display = "block";
+    taskContainer.innerHTML = '';
 }
 
 function check(element){
     if(element.checked){
-        element.parentNode.childNodes[3].style.textDecoration = "line-through red 3px";
+        element.parentNode.childNodes[3].style.textDecoration = "line-through #00ADB5 3px";
     }
     else{
         element.parentNode.childNodes[3].style.textDecoration = "none";
@@ -71,7 +101,7 @@ function deltask(element){
     localStorage.removeItem("todos");
     localStorage.setItem("todos", JSON.stringify(todos));
     let list = element.parentNode.parentNode;
-    list.removeChild(list.childNodes[1+index]);
+    list.removeChild(list.childNodes[index]);
     msganimate("Task Deleted");
     if(todos.length==0){
         empty.style.display = "block";
@@ -90,4 +120,21 @@ function msganimate(str){
         }
     }
     msg.innerHTML = str;
+}
+
+let prevtext;
+function updatetask(index){
+    inpTask.value = index.parentNode.childNodes[3].innerHTML;
+    prevtext = inpTask.value;
+    inpTask.focus();
+}
+
+function update(){
+    let index = todos.indexOf(prevtext);
+    taskContainer.childNodes[index].childNodes[3].innerHTML = inpTask.value;    
+    todos[todos.indexOf(prevtext)] = inpTask.value;
+    localStorage.removeItem("todos");
+    localStorage.setItem("todos", JSON.stringify(todos));
+    inpTask.value = '';
+    msganimate("Task Updated");
 }
